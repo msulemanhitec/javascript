15eaa16c9cf06a83417e6ec67f9bbdb30018bdde
msulemanhitec <m.sulemanhitec@gmail.com>
2024-02-25 11:11:59 -0800

    create  Question file

---
+++ Question.js
@@ -0,0 +1,1 @@
+console.log("Ma mare body ka content ha")
--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const accountId = 144553
 let accountEmail = "hitec@google.com"
 var accountpassword = "12345" 
 accountCity = "Jaipur"
+accountId = 2
+// console.log(accountId);
+ 
