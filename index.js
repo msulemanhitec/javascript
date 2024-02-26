6f111a6c45351cad3a1f215e0cf6bf4803959bb3
msulemanhitec <m.sulemanhitec@gmail.com>
2024-02-26 03:25:09 -0800

    create index.js file

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-console.log("variable consist declare")
+// Variable and Consit decalre kera ga 
 const accountId = 144553
-let accountEmail = "hitec@google.com"
-var accountpassword = "12345" 
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
 accountCity = "Jaipur"
-accountId = 2
-// console.log(accountId);
+ //accountId = 2 // not allow
+ console.log(accountId);
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+let acccountState;
+
+console.log(accountId);
+
+console.table ([accountId, accountEmail, accountPassword, accountCity, acccountState])
+
+
  
+
