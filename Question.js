439686c4c488c81f9a3e2c15c576d59e2ce9becb
msulemanhitec <m.sulemanhitec@gmail.com>
2024-02-27 03:50:09 -0800

    create test again file

--- Question.js
+++ Question.js
@@ -1,1 +1,26 @@
-console.log("Ma mare body ka content ha")
+"use strict"; // treat all js code as newer version
+// alert (3+3) // we are using nodejs, not browser
+//code readability should be high
+console.log("Hitesh") //output
+
+let name = "hitech"
+let age = 18
+let isloggedIn = false
+//Premittive Data type
+//number => 2 to power 53 // number range hota ha
+// bigint // Number both zida para ha speical(treating , trade markeding , vated , facebook ) hum jo bana ga itna number consume ni kara ga)
+string => ""
+//boolean (True , false) ( logdin , Ni Logdin ,  Created Detail Ha ni , Ya service sa reponse hia ya ni)
+//null => standalone Value
+//null Repsention Empty Value Ka // let state = null ( kali ha)
+//undefine => Apna Value define Ni kare
+let state; // ( Value define Ni Ha)
+//symbol => unique 
+//ya hamra ho aga premmitive data type
+
+//object
+ console.log(typeof "hitech"); //=> string
+ //console ka hader type ha wo ha ("") => string
+ console.log(typeof age); //=> string
+ console.log(typeof null); //=> object
+ console.log(typeof undefined);//=> undefined
