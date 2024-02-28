94115c0dfc06e7be2b5994790e7343b1127cc0eb
msulemanhitec <m.sulemanhitec@gmail.com>
2024-02-28 03:25:24 -0800

    create testagain file

--- testagain.js
+++ testagain.js
@@ -1,1 +1,81 @@
-console.log("test again")
+// let score ="33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// 
+// let score =null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// let score =undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// 
+// let score =undefined
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// let score =true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// Number 
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// let score =false
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// let score ="hitesh"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// // let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//Basic Conversion
+//"33" => 33
+// "33abc" => NaN  short forrm  (Not a Number) ,
+//NaN  ka types Number
+//true => 1; False => 0  
+//Boolean
+// let isLoggedIn=""
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let isLoggedIn="hitesh"
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// 1 => true;0 => False
+// ""=> false
+//"hitesh" =>  true
+
+// let someNumber =33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+//String
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+
+//Broswer string Value datha ha Bollen ,string , number 
+//someNumber display kara ga 
+ 33
+ //string Number display : output => string
