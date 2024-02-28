a8121bbe3e9825f07220eef773a74f6fc534547e
msulemanhitec <m.sulemanhitec@gmail.com>
2024-02-28 10:25:09 -0800

    create test file

--- test.js
+++ test.js
@@ -1,1 +1,53 @@
-console.log("Hello from test hello")
+// console.log("Hello from test hello")
+// ********************Operations***************
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " hitesh"
+
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" +2)
+// console.log(1+"2")
+// console.log("1" +2 +2);
+// console.log(1+2 +"2");
+// console.log(+true); //true Bolean  
+// console.log(+"");
+//  let num1,num2, num3
+// num1 = num2 = num3 = 2 +  2 //Better ways variable Define kerna ka
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);//his example koee change Ni deka
+//presiting operator (prefix kia hotha ha and postfix kia hotha ha)
+//Prefix ma value pala increment ho jathi ha 
+//postfix ma bad ma value increment ho jathi ha
+//Postfix increment
+//js
+// let x = 3;
+// const y=x++;
+// // x is 4;  y is 3
+// let x2 = 3n;
+// const y2 = x2++;
+//x2 is 4n;  y2 is 3n
+//Prefix increment
+JS
+let x = 3;
+const y = ++x;
+// x is 4; y is 4
+
+let x2 = 3n;
+const y2 = ++x2;
+// x2 is 4n; y2 is 4n
+//Link to study
+//coneratio operator
