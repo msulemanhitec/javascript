e3a0cc600a9bccfe2d5d1aa5b6524b56e2d0e605
msulemanhitec <m.sulemanhitec@gmail.com>
2024-03-01 13:31:34 -0800

    create Task file

---
+++ Task.js.js
@@ -0,0 +1,44 @@
+//Data Types of javascript
+// Data Types  ke dat Catogries
+// Premmitive bake sub ko Non primmitive
+// interview 
+// kis thra data types ko memory ma raka jatha ha or Access kia jatha ha
+// His basic data types  kp catrorization ha
+// Primitive 
+// Non Primitive
+// Reference  Type be Bola jatha ha
+// Primitive (Call By value  copy kartaha ha orgin data Wo refence Memory ka ni dia hin ko copy ilug dia jatha ha jo be chaning kertha ha copy ma  ilug sa changing kertha gaa copy ma chaning hotha ha )
+// 7 types : string , Number , Boolean, null , undefined , symbol (kise value ko unique bana ka lia symbol use hotha ha) , (symbol ka under wrap kerdatha ha uniqe bun jatha ha ) , 
+// Biglnt Scientfic handel ni gho rhee hothi  Bigint use kerlatha ha)
+//Integer Data Types 
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false;
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+const bigNumber = 3465435766544356754n
+console.log(id === anotherId);
+//Refence\Non Primative)\
+// Array , Objects , Functions
+const heros = ["shaktiman", "naagraj", "dog"] // Hin sub ka Rreference type Non primittive type)
+// (myobjvariable mastore ker saktha ha)
+let myobj = {
+    name: "hitesh",   //object 
+    age: 22,
+}
+//simple Function
+const myFunctionn = function () {
+    console.log("Hello world");
+}
+
+myFunctionn()
+// console.log(typeof bigNumber);
+
+
+// consit score =100 jo value da da
+// java script ,kabi Language ma defie ni kertha
+
+
