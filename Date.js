d4492a7f20d70849f971cf37845d69407dc42657
msulemanhitec <m.sulemanhitec@gmail.com>
2024-03-12 14:29:00 -0700

    create file Number.js

---
+++ Date.js
@@ -0,0 +1,19 @@
+ let myDate = new Date()
+// console.log(myDate); //date compare ni ker saktha use be ni ker saktha
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate) // his Date ka type dakna chatha hu Date(Object) javascript ka under
+
+//Month jo ha wo 0 sa start htha ha
+// let myCreateDate = new Date(2023, 0 , 23,5,3)  //new date spefic hothi ha (new Date create hothi ha)
+// let myCreateDate = new Date(2023 ,0 ,23 ,5,3)
+// console.log(myCreateDate.toDateString()); //Method bulid in
+// console.log(myCreateDate. toLocaleString());
+// let myCreatedate = new Date("2023-01-14")
+// console.log(myCreateDate.toLocaleString());
+
+// console.log(new Date()) //universal date
+// console.log(new Date().toLocaleString()) // new date time new new cheze find ho ge 
+
+let myCreatedDate = new Date("2023-01-14")
+console.log(myCreatedDate); //Local in (my Libary )
--- Number.js
+++ Number.js
@@ -1,13 +1,41 @@
  const score = 400
- console.log(score);
+//  console.log(score);
  const balance = new Number(100)
- console.log(balance);
- console.log(balance.toString());
+//  console.log(balance);
+//  console.log(balance.toString());
 
  console.log(balance.toString().length);
- console.log(balance.toFixed(1));
- console.log(balance.toFixed(2));// specical Ecommerence apploication bauna ga kie bar calculation hotha Mulplication hotha ha apna gs calculate kera procestion kuch zida pari ho jathi ha clint procede dekna ka hadad ni  his lia 2 Fix use kera specical E- Commerencew application buna rha ha 
+//  console.log(balance.toFixed(1));
+//  console.log(balance.toFixed(2));// specical Ecommerence apploication bauna ga kie bar calculation hotha Mulplication hotha ha apna gs calculate kera procestion kuch zida pari ho jathi ha clint procede dekna ka hadad ni  his lia 2 Fix use kera specical E- Commerencew application buna rha ha 
  
- const otherNumber = 23.8966
- console.log(otherNumber.toPrecision(3)); // Nymber of significant digits. Must be in the range1-21 inclusion
- 
+ const otherNumber = 1123.8966
+//  
+ const hundreds = 1000000
+//   console.log(hundreds.toLocaleString('en-IN'));
+ Number.MAX_VALUE
+ 1.
+//  const balance = new Number(100)
+//  console.log(balance);
+
+// +++++++++++++++++++++Maths  ++++++++++++++++++++++
+// Math subject or Math Libary javascript ka sath by deficult
+//  console.log(Math); 
+//  console.log(Math.abs(-4)); //abs absolute value negative balue postive ma jathi ha
+//   console.log(Math.round(4.6)); More round use kera ga (iround ik ka bad ik shaful hotha ratha ha)
+//   console.log(Math.ceil(4.2)); //top
+//   console.log(Math.floor(4.2)); lowerest value laga //0.6  ,.07 ,0.7point ma value lower value datha ha
+console.log(Math.min(4,3,6,8)); // array ka under sab sa lowest value konsa ha
+console.log(Math.max(4,3,6,8));
+console.log(Math.random()); // math .random ke value hamsa 0,1  (0.1,0.2 ho sakthi haka darmian ma hia ge //1-6,1-10
+// 1- Number ka lia rangardingha
+console.log((Math.random()*10) + 1); //Min 1 hia ge or 0 tho ni hia ge 
+console.log(Math.floor(Math.random()*10) + 1); //konsa 1 excute ho ga konsa second excute ho ga
+ //*10 value digit ho jathi ha Left ma shift ho jha ge
+const min =10
+const max = 20
+//1 digit chia Math .floor ho jho
+Math.random()
+console.log( Math.random() * (max -min + 1))
+ //tha ka  0 value na hia //Next Condition (Min 10 value do)
+ console.log(Math.floor(Math.random() * (max -min + 1)) + min) // max-min his range ma chia +1  kerna ka tha 0 case aviod ker du
+//  same value the + min laga dia add kia
